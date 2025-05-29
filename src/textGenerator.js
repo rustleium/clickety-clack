47b100b17872d73b673972e9a64a0def9e666acf
Rustam <189756814+rustleium@users.noreply.github.com>
2025-05-29 17:24:58 +0530

    feat: add function to generate random letter

---
+++ src/textGenerator.js
@@ -0,0 +1,7 @@
+export function generateText() {
+  const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  let randomNumber = Math.floor(Math.random() * 27);
+  return alphabets[randomNumber];
+}
+
