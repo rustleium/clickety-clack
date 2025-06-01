a623d785dc28e94ffb11fa0c8c331b74dd324da8
Rustam <189756814+rustleium@users.noreply.github.com>
2025-06-01 19:11:08 +0530

    fix: generate random number from 0 to 25
    Now, textGenerator() won't return undefined and so there won't be
    blank screen

--- src/textGenerator.js
+++ src/textGenerator.js
@@ -1,7 +1,7 @@
 export function generateText() {
   const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  let randomNumber = Math.floor(Math.random() * 27);
+  let randomNumber = Math.floor(Math.random() * 26);
   return alphabets[randomNumber];
 }
 
